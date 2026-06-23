bb93f51b5d6d33ec107d6529bebcd36024dc94d9
ISHANTRANA1 <ishantrana2005@gmail.com>
2026-06-23 13:37:14 +0530

    fix: minimal middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,2 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-
-export function middleware(request: NextRequest) {
-  return NextResponse.next();
-}
-
-export const config = {
-  matcher: [],
-};
+export function middleware() {}
+export const config = { matcher: [] };
